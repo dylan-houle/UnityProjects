a7b2ca0ddc0dd42cceb6711da344917af8351ac3
dylan-houle <67667140+dylan-houle@users.noreply.github.com>
2021-02-16 20:38:18 -0500

    c

--- Liminal/Assets/Scripts/PlayerMovement.cs
+++ Liminal/Assets/Scripts/PlayerMovement.cs
@@ -4,23 +4,44 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    
     public CharacterController controller;
 
-    public float speed = 12f;
-    public float gravity = -9.81f;
+    public float playerSpeed = 1;
+    public float gravity = 9.81f;
     public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
     
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Vector3 velocity;
+    bool isGrounded;
 
     // Update is called once per frame
     void Update()
     {
-        
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 move = contorller.right * x + contorller.forward * z;
+
+        controller.Move(move * playerSpeed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
